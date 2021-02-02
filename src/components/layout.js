bbad27184d250e70ce967d82b9935ccf2d14e10c
swaroop_a.s <swaroopanand325@gmail.com>
2021-02-02 21:49:07 +0530

    react helmet configured for open graph content

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,25 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
-
+import ProfileImage from '../images/profile_pic.jpg'
 import '../assets/scss/main.scss'
 
 const Layout = ({ children, location }) => {
-
-  let content;
+  let content
 
   if (location && location.pathname === '/') {
-    content = (
-      <div>
-        {children}
-      </div>
-    )
+    content = <div>{children}</div>
   } else {
     content = (
       <div id="wrapper" className="page">
-        <div>
-          {children}
-        </div>
+        <div>{children}</div>
       </div>
     )
   }
@@ -41,8 +34,22 @@ const Layout = ({ children, location }) => {
           <Helmet
             title={data.site.siteMetadata.title}
             meta={[
-              { name: 'description', content: 'Sample' },
-              { name: 'keywords', content: 'sample, something' },
+              { name: 'description', content: 'Swaroop portfolio' },
+              { name: 'keywords', content: 'swaroop, swaroop325' },
+              { name: 'twitter:site', content: 'Swaroop Portfolio' },
+              { name: 'twitter:creator', content: 'Swaroop.site' },
+              { name: 'twitter:title', content: 'Swaroop Portfolio' },
+              { name: 'twitter:image', content: ProfileImage },
+
+              { property: 'og:title', content: 'Swaroop A.S Portfolio' },
+              { property: 'og:type', content: 'website' },
+              {
+                property: 'og:url',
+                content: 'https://swaroop325.web.app/',
+              },
+              { property: 'og:description', content: 'Swaroop A.S Portfolio Website' },
+              { property: 'og:image', content: ProfileImage },
+              { property: 'og:site_name', content: 'Swaroop Portfolio' },
             ]}
           >
             <html lang="en" />
