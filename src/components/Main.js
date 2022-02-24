3b7d1b473581ab8ab92f507324f6d1f672fb0092
swaroop_a.s <swaroopanand325@gmail.com>
2022-02-25 03:10:53 +0530

    added experiences

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,9 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import ProfileImage from '../images/profile_pic.jpg'
 import Leader from '../images/leader.jpg'
-
+import GFT from '../images/gft.jpeg'
+import CFT from '../images/cft.jpeg'
+import Infy from '../images/infy.jpeg'
 class Main extends React.Component {
   render() {
     let close = (
@@ -32,10 +34,10 @@ class Main extends React.Component {
             <img src={ProfileImage} alt="" />
           </span>
           <p>
-            Passionate Mobile Application Developer and obsessed with React.js
-            and its frameworks. Focussed in Agile method of development and keen
-            on bug free delivery of product. Enthusiast in working on anything
-            new and trending in tech.
+            Passionate Web/Mobile Application Developer and obsessed with
+            React.js and its frameworks. Focussed in Agile method of development
+            and keen on bug free delivery of product. Tech Enthusiast and
+            interested in working on anything new and emerging in tech.
           </p>
           {close}
         </article>
@@ -48,11 +50,52 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Work</h2>
-          {/* <span className="image main">
-            <img src={pic02} alt="" />
-          </span> */}
-          <p>Infosys Limited</p>
-          <p></p>
+          <div className="workBlock">
+            <div>
+              <span className="workTitle">Intern - Member Technical Staff</span>
+              <span style={{ float: 'right' }}>Dec 2018 - Feb 2019</span>
+            </div>
+            <div>
+              <span className="workTitle">Member Technical Staff</span>
+              <span style={{ float: 'right' }}>Apr 2019 - Apr 2020</span>
+            </div>
+            <div>
+              <span>
+                <img className="workImage" src={GFT} alt="" />
+              </span>
+              <span>GoFrugal Technologies </span>
+              <span style={{ float: 'right' }}>Chennai, India</span>
+            </div>
+          </div>
+          <div className="workBlock">
+            <div>
+              <span className="workTitle">React JS Developer</span>
+              <span style={{ float: 'right' }}>Jun 2020 - Nov 2020</span>
+            </div>
+            <div>
+              <span className="workTitle">Tech Lead - Frontend Team</span>
+              <span style={{ float: 'right' }}>Dec 2020 - May 2021</span>
+            </div>
+
+            <div>
+              <span>
+                <img className="workImage" src={CFT} alt="" />
+              </span>
+              <span>Craftveda Technology Private Limited</span>
+              <span style={{ float: 'right' }}>Kolkata, India (Remote)</span>
+            </div>
+          </div>
+          <div className="workBlock">
+            <span className="workTitle">Associate Consultant</span>
+            <span style={{ float: 'right' }}>Jun 2021 - Present</span>
+            <div>
+              <span>
+                <img className="workImage" src={Infy} alt="" />
+              </span>
+              <span>Infosys Limited</span>
+              <span style={{ float: 'right' }}>Trivandrum, India</span>
+            </div>
+          </div>
           {close}
         </article>
 
